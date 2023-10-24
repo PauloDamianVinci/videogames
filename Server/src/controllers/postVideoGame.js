4c3ffb1a7df6bfbf61501d929602e665269b3d89
PauloDamianVinci <paulodamianvinci@gmail.com>
2023-10-24 18:46:58 -0300

    post videogame

--- Server/src/controllers/postVideoGame.js
+++ Server/src/controllers/postVideoGame.js
@@ -1,40 +1,39 @@
-// const { Favorite } = require('../DB_connection');
-// const { User } = require('../DB_connection');
-// const getAllFav = require("./getAllFav");
+const { Videogame } = require('../DB_connection');
+const { Genre } = require('../DB_connection');
+const { Platform } = require('../DB_connection');
 const showLog = require("../functions/showLog");
 
 const postVideoGame = async (req, res) => {
-    //const { userId, id, name, origin, status, image, species, gender } = req.body;
+    const { name, description, image, released_date, rating, platform, genre } = req.body;
     try {
-        // if (id && userId && name && origin && status && image && species && gender) {
-        //     showLog("postFav id ", id, ", user  ", userId);
-        //     // Busco el usuario:
-        //     const user = await User.findByPk(userId);
-        //     if (!user) { throw Error("user not found"); }
-        //     // Usuario encontrado. Busco el favorito:
-        //     const [favorite, created] = await Favorite.findOrCreate({
-        //         where: {
-        //             id,
-        //             name,
-        //             status,
-        //             species,
-        //             gender,
-        //             origin,
-        //             image,
-        //         },
-        //     });
-        //     // Asocio el favorito al usuario si no estaba asociado previamente:
-        //     await user.addFavorite(favorite);
-        //     // Devuelvo todos los favoritos del usuario:
-        //     const allFav = await getAllFav(userId);
-        //     return res.status(200).json(allFav);
-        // } else {
-        //     showLog("postFav id = all, user  ", userId);
-        //     // si no me carga los datos sólo devuelvo lo que tengo. Es para que al iniciar el programa, se traiga los fav almacenados para mostrar.
-        //     // Devuelvo todos los favoritos del usuario:
-        //     const allFav = await getAllFav(userId);
-        //     return res.status(200).json(allFav);
-        // }
+        if (!name || !description || !image || !released_date || !rating || !platform || !genre) { throw Error("Data missing"); }
+        // Verifico repeticón de nombre:
+        const esRepe = await Videogame.findOne({
+            where: { name: name, },
+        });
+        if (esRepe) { throw Error("It already exists"); }
+
+        const [VideogameCreated, created] = await Videogame.findOrCreate({
+            where: {
+                name,
+                description,
+                image,
+                released_date,
+                rating,
+            },
+        });
+
+        let genreCreated = await Genre.findAll({
+            where: { name: genre }
+        });
+
+        let platformCreated = await Platform.findAll({
+            where: { name: platform }
+        })
+
+        VideogameCreated.addGenre(genreCreated);
+        VideogameCreated.addPlatform(platformCreated);
+        return res.status(200).json({ "created": "ok" });
     } catch (err) {
         showLog(`ERROR-> ${err.message}`);
         return res.status(500).send(err.message);
