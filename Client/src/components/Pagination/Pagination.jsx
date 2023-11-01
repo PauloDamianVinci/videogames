a6b0bb99b374b7c5d50b978c6cc3360a1ea78af3
PauloDamianVinci <paulodamianvinci@gmail.com>
2023-10-31 21:45:28 -0300

    style paginado en progreso

--- Client/src/components/Pagination/Pagination.jsx
+++ Client/src/components/Pagination/Pagination.jsx
@@ -1,6 +1,6 @@
 // Estilos:
 import style from "./Pagination.module.css";
-const { paginados, activePage } = style;
+const { paginados, activePage, container, contButton, button } = style;
 
 const Pagination = (props) => {
     const { videogamePerPage, allVideogames, paginado, currentPage } = props;
@@ -12,22 +12,23 @@ const Pagination = (props) => {
 
     return (
         <div>
-            <nav className={paginados}>
-                <ul>
-                    {pgNum.map((number) => {
-                        return (
-                            <li key={number}>
-                                <a href={`#${number}`}
-                                    className={number === currentPage ? activePage : ""}
-                                    onMouseDown={() => paginado(number)}>
+            <ul className={container}>
+                {pgNum.map((number) => {
+                    return (
+                        <li key={number}>
+                            <div className={contButton}>
+                                <button
+                                    onClick={() => paginado(number)}
+                                    className={number === currentPage ? button : ""}
+                                >
                                     {number}
-                                </a>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </nav>
+                                </button>
+                            </div>
 
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 
@@ -35,3 +36,23 @@ const Pagination = (props) => {
 }
 
 export default Pagination;
+// return (
+//     <div>
+//         <nav className={paginados}>
+//             <ul>
+//                 {pgNum.map((number) => {
+//                     return (
+//                         <li key={number}>
+//                             <a href={`#${number}`}
+//                                 className={number === currentPage ? activePage : ""}
+//                                 onMouseDown={() => paginado(number)}>
+//                                 {number}
+//                             </a>
+//                         </li>
+//                     );
+//                 })}
+//             </ul>
+//         </nav>
+
+//     </div>
+// );
